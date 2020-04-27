37fd35dd69e55796eb85d1c8e56599c5edd8ff9a
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-04-27 20:58:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 // Write a function that modifies the original object and updates object with the given key-value. It should return the entire updated object
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  return object[key] = value;
+  return object.key = value;
 }
