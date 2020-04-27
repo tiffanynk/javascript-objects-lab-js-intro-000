7c5184d3d596a93836929f53c11e552ced6a86e1
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-04-27 21:02:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Write a function that does not modify the original object and returns an object with the orignal key value pairs and the new key value pair
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, {
-    [key]: value })
+  return Object.assign({}, object, { [key]: value })
 }
 
 // Write a function that modifies the original object and updates object with the given key-value. It should return the entire updated object
